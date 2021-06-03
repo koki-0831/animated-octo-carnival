8d7c24f8949b3bbf0914c31a44573c22e973fcf3
koki-0831 <84952888+koki-0831@users.noreply.github.com>
2021-06-03 14:43:13 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function 曲1(){
+    alert("2021/5/28に配信された最新曲の1曲で6/4に公開される「るろうに剣心 The Beginning」の主題歌。現在この曲のMVを一般から募集しているので応募してみたい。")
+}
+
+function 歌詞1(){
+    window.open('https://www.uta-net.com/song/302753/');
+}
+
+function 曲2(){
+    alert("2021/4/16に配信された最新曲の1曲で4/23に公開される「るろうに剣心 The Final」の主題歌。ワンオクらしい激しいロックな曲でとてもかっこいい。")
+}
+
+function 歌詞2(){
+    window.open('https://www.uta-net.com/song/301105/');
+}
+
+function 曲3(){
+    alert("2018/11/23に配信された曲でONE OK ROCKというバンドを知るきっかけとなった1曲。 個人的にこの曲はワンオクの中で最も好きな曲のうちの1曲です。")
+}
+
+function 歌詞3(){
+    window.open('https://www.uta-net.com/song/259349/');
+}
